e77fdd144e7302b43b0ff689263113f86a787dac
davechai <davecqx@gmail.com>
2019-10-21 17:22:21 +0800

    WIP

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <template>
   <div id="app">
-    <ul>
+    <h3 v-if="loading">Loading...</h3>
+    <ul v-if="!loading">
       <li>
-        <img src="https://gss3.bdstatic.com/7Po3dSag_xI4khGkpoWK1HF6hhy/baike/c0%3Dbaike180%2C5%2C5%2C180%2C60/sign=8de7d69cb87eca80060831b5f04afcb8/e61190ef76c6a7eff1edd899f5faaf51f2de6641.jpg" alt="">
+        <img style="width: 100%;" src="https://gss3.bdstatic.com/7Po3dSag_xI4khGkpoWK1HF6hhy/baike/c0%3Dbaike180%2C5%2C5%2C180%2C60/sign=8de7d69cb87eca80060831b5f04afcb8/e61190ef76c6a7eff1edd899f5faaf51f2de6641.jpg" alt="">
+        <div class="list-footer">
+          <span>朴信惠</span>
+          <button>Like</button>
+        </div>
       </li>
     </ul>
   </div>
@@ -13,6 +18,12 @@
 
   export default {
     name: 'app',
+    data() {
+      return {
+        loading: true,
+        all_videos: []
+      }
+    },
     apollo: {
       all_videos: gql`query {
          allVideos {
@@ -22,6 +33,16 @@
     },
     methods: {
 
+    },
+    async mounted() {
+      this.all_videos = await this.$apollo.query({
+        query: gql`query {
+          allVideos {
+            link
+          }
+        }`
+      })
+      this.loading = false
     }
   }
 </script>
@@ -33,6 +54,17 @@
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
-    margin-top: 60px;
+  }
+
+  #app ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+
+  .list-footer {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
   }
 </style>
